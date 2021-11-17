f8a57d207911954ce191afa7dd8f54cc0483792e
Diogo Pina <diogojpina@gmail.com>
2021-11-17 19:38:38 -0300

    feat: Export files stats

---
+++ src/command/stats/files-stats.ts
@@ -0,0 +1,76 @@
+import { Command } from 'commander'
+import mongoose from 'mongoose'
+
+import { Repository } from '../../model/Repository'
+
+import { GithubService } from '../../services/GithubService'
+import { SonarService } from '../../services/SonarService'
+
+import { createArrayCsvWriter } from 'csv-writer'
+import ObjectsToCsv from 'objects-to-csv'
+
+import path from 'path'
+import { config } from 'dotenv'
+import { stat } from 'fs'
+
+config({ path: path.join(__dirname, '../../.env') })
+
+class FilesStats {
+  public async run (options: any) {
+    const octokit = await GithubService.getOctokit()
+
+    const sonarService: SonarService = new SonarService()
+
+    const measures = await sonarService.getFilesOverallMeasures()
+
+    const rows = []
+    let stats: any = {}
+    let uuid = ''
+    for (const measure of measures.rows) {
+      if (uuid !== measure.uuid) {
+        if (stats?.uuid) {
+          if (stats.measure_lines !== undefined) {
+            rows.push(stats)
+          }
+
+          stats = {}
+        }
+
+        uuid = measure.uuid
+        stats.uuid = measure.uuid
+        stats.long_name = measure.long_name
+      }
+      stats[`measure_${measure.name}`] = parseInt(measure.value)
+      // console.log('measure', measure.uuid)
+    }
+    if (stats.measure_lines !== undefined) {
+      rows.push(stats)
+    }
+
+    console.log(rows.length)
+
+    console.log('create-csv-object')
+    const csv = new ObjectsToCsv(rows)
+    console.log('csv-object-created')
+
+    console.log('writing')
+    await csv.toDisk('./files-stats.csv')
+    console.log('wrote')
+
+    process.exit(0)
+  }
+}
+
+const program = new Command()
+program.version('0.0.1')
+
+program
+  .option('-lang, --language <language>', 'language')
+  .option('-l, --limit <limit>', 'Quantity of repositories to explrorer')
+
+program.parse(process.argv)
+
+const options = program.opts()
+
+const fileStats = new FilesStats()
+fileStats.run(options)
--- src/services/SonarService.ts
+++ src/services/SonarService.ts
@@ -127,6 +127,18 @@ export class SonarService {
     return stats
   }
 
+  public async getFilesOverallMeasures (): Promise<any> {
+    const query = `select c.uuid, c.long_name, lm.metric_id, m.name, m.short_name, m.description, m.domain, m.val_type, lm.value, lm.text_value from live_measures lm 
+    inner join metrics m on m.id = lm.metric_id 
+    inner join components c on c.uuid = lm.component_uuid 
+    where c."language" = 'java' and c."scope" = 'FIL' and c.qualifier  = 'FIL' and 
+    (m.val_type = 'FLOAT' or m.val_type = 'INT' or m.val_type = 'PERCENT' or m.val_type = 'RATING')    
+    order by c.uuid`
+    const res = await this.pool.query(query)
+
+    return res
+  }
+
   public async getProjectOverallMeasures (projectKee: string): Promise<any> {
     const query = `select pm.metric_id, m.name, m.short_name, m.description, m.domain, m.val_type, pm.value, pm.text_value
     from project_measures pm 
